d99994a2a641941c4b6a43dc584ac36f6b931c7e
Alexey Noskov <vokson@uandex.ru>
2018-01-21 15:39:21 +0300

    Title changed

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>TEST</title>
+    <title>Чертежи АГПЗ</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 
